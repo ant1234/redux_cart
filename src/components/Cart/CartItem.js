e70b1ee52ecc461dce84b212632d87736224b28b
unknown <antonyvidetta1986@gmail.com>
2023-08-08 14:41:44 +1200

    Add and remove from cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,27 @@
 import classes from './CartItem.module.css';
+import { cartActions } from '../../store/cart-slice';
+import { useSelector, useDispatch} from 'react-redux';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+
+  const dispatch = useDispatch();
+
+  const addItemHandler = () => {
+    dispatch(cartActions.addItem({
+      id,
+      title, 
+      quantity, 
+      total, 
+      price
+    }));
+  };
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItem({
+      id
+    }));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +37,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-createSlice({
+const cartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [],
@@ -10,9 +10,10 @@ createSlice({
         addItem(state, actions) {
             const newItem = actions.payload;
             const existingItem = state.items.find(item => item.id === newItem.id);
+            state.totalQuantity++;
             if(!existingItem) {
                 state.items.push({
-                    itemId: newItem.id,
+                    id: newItem.id,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price, 
@@ -24,7 +25,21 @@ createSlice({
             }
         }, 
         removeItem(state, actions) {
-
+            const cartItem = actions.payload;
+            const existingItem = state.items.find(item => item.id === cartItem.id);
+            state.totalQuantity--;
+            if(existingItem.quantity === 1) {
+                console.log('inside the if');
+                console.log(existingItem.quantity);
+                state.items = state.items.filter(item => item.id !== cartItem.id);
+            } else {
+                existingItem.quantity--;
+                console.log('inside the else');
+                console.log(existingItem.quantity);
+            }
         }
     }
 });
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
