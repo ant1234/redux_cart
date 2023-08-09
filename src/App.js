3487f4999528a0f6f4a381d51df581fc85ffbf83
unknown <antonyvidetta1986@gmail.com>
2023-08-09 18:15:59 +1200

    Add http request when the cart is changed in anyway with a react side effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Cart from './components/Cart/Cart';
+import { useEffect } from 'react';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
@@ -6,6 +7,14 @@ import { useSelector } from 'react-redux';
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisable);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-http-f885f-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT', body: JSON.stringify(cart)
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
